fc024d07b72435b0f16a01a14a4d44efd1758968
Travis Miller <travismillerdes@gmail.com>
2025-10-09 15:11:36 -0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,19 @@
 // .eleventy.js
 
 module.exports = function(eleventyConfig) {
-    // Set the input (source) directory to 'src'
-    // and the output (compiled) directory to 'dist'
+    // Tells Eleventy to use Nunjucks for files ending in .html
+    eleventyConfig.setLiquidOptions({
+        dynamicPartials: false,
+        strictFilters: false, // will prevent crashing on undefined filters
+    });
+
     return {
       dir: {
         input: "src",
         output: "dist",
-        // The default layout folder is '_includes' inside the input folder
-        // so Eleventy will automatically look in 'src/_includes'
       },
+      // THIS IS THE KEY CHANGE
+      htmlTemplateEngine: "njk", 
+      markdownTemplateEngine: "njk"
     };
   };
