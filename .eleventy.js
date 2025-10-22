ffdb52d94db2ce2f2fca5c3893569928def42418
Travis Miller <travismillerdes@gmail.com>
2025-10-22 10:37:02 -0700

    Improve SCSS compiling

--- .eleventy.js
+++ .eleventy.js
@@ -7,8 +7,20 @@ module.exports = function(eleventyConfig) {
         strictFilters: false, // will prevent crashing on undefined filters
     });
 
+    // Copy CSS and scripts to output during watch mode (passthrough during serve)
     eleventyConfig.addPassthroughCopy("src/styles/project.css");
     eleventyConfig.addPassthroughCopy("src/scripts");
+
+    // Watch the compiled CSS file and trigger browser reload when it changes
+    eleventyConfig.addWatchTarget("src/styles/project.css");
+
+    // Enable passthrough copy behavior to work during --serve
+    eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+
+    // Set browser sync to reload on CSS changes
+    eleventyConfig.setServerOptions({
+        watch: ["dist/styles/project.css"]
+    });
 
     return {
       dir: {
@@ -16,7 +28,7 @@ module.exports = function(eleventyConfig) {
         output: "dist",
       },
       // THIS IS THE KEY CHANGE
-      htmlTemplateEngine: "njk", 
+      htmlTemplateEngine: "njk",
       markdownTemplateEngine: "njk"
     };
   };
